b4bfe59c438d810c25003b10ce9ccf600566720e
MrKomugiko <Mr.Komugiko@gmail.com>
2020-07-13 19:01:45 +0200

    Binding Player statistics to window

---
+++ Engine/ViewModel/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModel
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public GameSession() {
+            CurrentPlayer = new Player() {
+                Name = "Kamil",
+                CharacterClass = "Rougue",
+                HitPoint = 100,
+                ExperiencePoints = 0,
+                Level = 1,
+                Gold = 1000000
+            };
+
+        }
+    }
+}
