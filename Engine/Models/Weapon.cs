7c99e5def9d8f67360f27c56986bd1a1c66d0cc3
MrKomugiko <Mr.Komugiko@gmail.com>
2020-07-13 22:28:45 +0200

    Creating a Item system

---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximimDamage { get; set; }
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) 
+            : base(itemTypeID, name, price) {
+            MinimumDamage = minDamage;
+            MaximimDamage = maxDamage;
+        }
+
+        public new Weapon Clone() {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximimDamage);
+        }
+    }
+}
