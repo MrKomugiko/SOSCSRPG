7e38d0872f385e9d6da4d78093781c173d63bbd7
MrKomugiko <Mr.Komugiko@gmail.com>
2020-07-14 14:03:18 +0200

    monsters, messaging

--- Engine/Models/MonsterEncounter.cs
+++ Engine/Models/MonsterEncounter.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Engine.Models
+﻿namespace Engine.Models
 {
-    public  class MonsterEncounter
+    public class MonsterEncounter
     {
         public int MonsterID { get; set; }
         public int ChanceOfEncountering { get; set; }
+
         public MonsterEncounter(int monsterID, int chanceOfEncountering) {
             MonsterID = monsterID;
-            ChanceOfEncountering = ChanceOfEncountering;
-
+            ChanceOfEncountering = chanceOfEncountering;
         }
     }
 }
